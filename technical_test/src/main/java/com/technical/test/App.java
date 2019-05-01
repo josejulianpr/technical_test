198464919fe335e93d5805bf0138b071294f8bfd
José Julián <josejulianprado@gmail.com>
2019-05-01 16:18:51 -0400

    Commit Inicial

---
+++ technical_test/src/main/java/com/technical/test/App.java
@@ -0,0 +1,13 @@
+package com.technical.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
